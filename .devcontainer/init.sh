8dc5ef3f6c28eb40c0225ef8851855a73bb03d8a
Oscar Söderlund <oscar@nebulr.group>
2022-01-11 16:10:38 +0100

    Dev docker container support

---
+++ .devcontainer/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## Usage
+## ./init_local.sh WORKSPACE_FOLDER_PATH
+PWD=$(dirname $0)
+source $PWD/secrets.env
+
+echo "Installing vim + sudo"
+apt-get update
+apt-get -y install vim sudo
+
+echo "Configuring GIT"
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+
+echo "Done dev specific initializing, running workspace_folder/init.sh..."
+. $1/init.sh
---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## This file setups all prerequisites for the docker container, used both dev environment and in CI/CD pipeline.
+## For prerequisites strictly related to dev environment see ./.devcontainer/init.sh which is run before this script.
+
+sudo npm install
+
+echo "Done initializing"
