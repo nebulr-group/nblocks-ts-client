5b36497d5ffffda25a446004ea85f537908ea6d2
nemanja255 <112646463+nemanja255@users.noreply.github.com>
2022-09-15 16:16:22 +0200

    Improvements and Fixes for docker, dependabot, and devcontainer config (#11)
    * Add env context var for naming Docker image
    
    * Update dependabot target-branch to be stage
    
    * Update Dockerfile default shell
    
    * Change name of publish.sh => npm-config-setter.sh & factorize the changes in ci-cd.yml
    
    * Update devcontainer config settings for workspaceMount and workspaceFolder
    
    * Add less, jq and curl utils to the init.sh apt install
    
    * Remove sudo npm install from root init.sh, Dockerfile contains this step
    
    * Set vim as core.editor for git tool
    
    * Fix name typo for env context var

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -4,11 +4,12 @@
 PWD=$(dirname $0)
 source $PWD/secrets.env
 
-echo "Installing vim + sudo"
+echo "Installing utils using apt..."
 apt-get update
-apt-get -y install vim sudo
+apt-get -y install vim less jq curl
 
 echo "Configuring GIT"
+git config --global core.editor "vim"
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 
