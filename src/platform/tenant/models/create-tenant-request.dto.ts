e4a8530a9e5318b0aaaba0b757f115c12d106238
Oscar Söderlund <oscar@nebulr.group>
2022-12-04 21:58:12 +0100

    Mute notifications for new tenant owner

--- src/platform/tenant/models/create-tenant-request.dto.ts
+++ src/platform/tenant/models/create-tenant-request.dto.ts
@@ -1,16 +1,16 @@
 export class CreateTenantRequestDto {
     /** The plan must be present in the configured App business model */
     plan?: string;
-  
+
     /** A user will need to be assigned as an owner to this tenant. Either reused if the user already exist or a new user will be created. The user will be onboarded automatically */
     owner: TenantOwnerRequestDto
-    
+
     /** Name of the tenant. Can also be set by tenant owner during onboarding */
     name?: string;
-    
+
     /** A url to a logo. Can also be set by tenant owner during onboarding */
     logo?: string;
-    
+
     /** Store metadata for your own business logic that will be returned in every Tenant response. This data will never be outputted to the end user */
     metadata?: Record<string, string>;
 }
@@ -18,6 +18,13 @@ export class CreateTenantRequestDto {
 export class TenantOwnerRequestDto {
     /** Required */
     email: string;
+
+    /** The first name */
     firstName?: string;
+
+    /** The last name */
     lastName?: string;
+    
+    /** Set this variable to true if you don't wish to send out any notifications to the new user */
+    muteNotifications?: boolean;
 }
