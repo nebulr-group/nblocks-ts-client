995c17d3dae45464535c8d083a2ec47f8a03f749
Oscar Söderlund <oscar@nebulr.group>
2023-10-07 16:39:50 +0200

    Conforming to new tenant output. Released as 3.0.0-payment-alpha.9

--- src/platform/tenant/models/tenant-payment-status.ts
+++ src/platform/tenant/models/tenant-payment-status.ts
@@ -1,4 +1,10 @@
 export interface TenantPaymentStatus {
-    shouldSelectPlan: boolean;
+    /** Payment method is set up. We can bill this tenant */
+    paymentsEnabled: boolean;
+
+    /** The tenant should setup payment immediately because tenant has not setup payments and the subscribing plan carry a cost and the trial has ended */
     shouldSetupPayments: boolean;
+
+    /** The tenant should immediately select a plan */
+    shouldSelectPlan: boolean
 }
--- src/platform/tenant/models/tenant.model.ts
+++ src/platform/tenant/models/tenant.model.ts
@@ -1,40 +1,39 @@
+import { TenantPaymentStatus } from "./tenant-payment-status";
+
 /**
  * A tenant represents the account or workspace of a customer and encapsulate users for that workspace. A new tenant always creates an `OWNER` user that should be granted to change name, locale and manage the subscription (if using payment service).
  */
- export class TenantResponseDto {
+export class TenantResponseDto {
 
-   /** Logical id reference to this instance */
-   id: string;
+  /** Logical id reference to this instance */
+  id: string;
 
-   /** Name of the subscription plan. Automatically set by the payment service if tenant used the checkout process */
-   plan?: string;
+  /** Name of the subscription plan. Automatically set by the payment service if tenant used the checkout process */
+  plan?: string;
 
-   /** The default locale / lang code for all users in this tenant (`ISO_639-1` format). 
+  /** MFA enabled */
+  mfa: boolean;
+
+  /** Payment status */
+  paymentStatus: TenantPaymentStatus;
+
+  /** The default locale / lang code for all users in this tenant (`ISO_639-1` format). 
    * This property will set the i18n for all platform emails and can be used to 
-   */
-   locale: string;
+  */
+  locale: string;
 
-   /** Name of the tenant. Undefined from the begining until the Tenant owner complets tenant onboarding. */
-   name?: string;
+  /** Name of the tenant. Undefined from the begining until the Tenant owner complets tenant onboarding. */
+  name?: string;
 
-   /** Url to a public accessible logo */
-   logo?: string;
+  /** Url to a public accessible logo */
+  logo?: string;
 
-   /** Require users to login with MFA/2FA */
-   mfa: boolean;
+  /** Store app specific meta data for this tenant */
+  metadata: Record<string, string>;
 
-  /** Payment method is set up. We can bill this workspace */
-   paymentsEnabled: boolean;
+  /** If this tenant is considered onboarded (Read only) */
+  onboarded: boolean;
 
-  /** The tenant should setup payment immediately because tenant has not setup payments and the subscribing plan carry a cost and the trial has ended */
-   paymentsRequired: boolean;
-
-   /** Store app specific meta data for this tenant */
-   metadata?: Record<string, string>;
-
-   /** If this tenant is considered onboarded (Read only) */
-   onboarded: boolean;
-
-   /** Timestamp when created */
-   createdAt: Date;
- }
+  /** Timestamp when created */
+  createdAt: Date;
+}
