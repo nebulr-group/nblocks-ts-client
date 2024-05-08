7043edc5fb8329f10bab18daf92fe62a0d13974c
Oscar Söderlund <oscar@nebulr.group>
2024-05-08 09:39:01 +0200

    Added new properties to user update

--- src/platform/tenant/user/models/update-user-request.dto.ts
+++ src/platform/tenant/user/models/update-user-request.dto.ts
@@ -1,5 +1,25 @@
 export class UpdateUserRequestDto {
-  role: string;
-  enabled: boolean;
-  teams: string[];
+  /** The role key. Must exists as a Role */
+  role?: string;
+
+  /** Enable / Disable access to your app */
+  enabled?: boolean;
+  
+  /** Organize the user into teams with key identifiers */
+  teams?: string[];
+  
+  /** User's first name */
+  firstName?: string;
+  
+  /** User's last name */
+  lastName?: string;
+  
+  /** User's phone number */
+  phoneNumber?: string;
+  
+  /** Mark user as onboarded */
+  onboarded?: boolean;
+  
+  /** Mark user has consent to your privacy policy */
+  consentsToPrivacyPolicy?: boolean;
 }
